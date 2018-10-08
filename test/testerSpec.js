525723ad23866e46d067653603c152d72fae4005
Jackie <computer@home>
2018-10-08 19:24:25 +1100

    Port tester functional.

--- test/testerSpec.js
+++ test/testerSpec.js
@@ -1,17 +1,12 @@
-var app = require("../controllers/application");
-var userAuth = require("../routes/authentication")
+describe("Website", function() {
 
-describe("getQuestions", () => { //TESTING THE TEST
-    it("Getting Questions", () => {
-         var result = app.getQuestions.length;
-         expect(app.getQuestions.length).toBe(result);
-        
-    });
-});  
-describe("Login", () => {
-    it("User Login", () =>{
-        var username = bob;
-        var password = password123
-        
+    var app = require("../controllers/application");
+    var loginInfo = require("../controllers/authentication");
+    var userAuth = require("../routes/authentication");
+    var loanApp = require("../routes/application");
+    var port = require("../config/index");
+    it("Correct port", () => {
+        var portNum = 4000;
+        expect(port.DEV_PORT).toBe(portNum)
     });
 });
