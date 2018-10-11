7a0d71dbfdc141e8c5b9daf25334b42191dc1cca
Jackie <computer@home>
2018-10-11 15:03:26 +1100

    More work on Unit testing

--- test/testerSpec.js
+++ test/testerSpec.js
@@ -1,12 +1,26 @@
-describe("Website", function() {
+var port = require("../config/index");
+var Request = require("request");
+//var app = require("../controllers/application");
+var server;
+beforeAll(() => {
+    server = require("../index");
 
-    // var app = require("../controllers/application");
-    // var loginInfo = require("../controllers/authentication");
-    // var userAuth = require("../routes/authentication");
-    // var loanApp = require("../routes/application");
-    var port = require("../config/index");
+});
+var data = {};
+beforeAll((done) => {
+    Request.get("http://localhost:4000/", (req, res, next) => {
+            data.status = res.statusCode;
+            data.next = next;
+            done();
+    });
+});
+// Test cases begin
+describe("Website", function() {
     it("Correct port", function() {
         var portNum = 4000;
         expect(port.DEV_PORT).toBe(portNum)
+    });
+    it("Status 200 - Successful connection", function(){
+        expect(data.status).toBe(200);
     });
 });
