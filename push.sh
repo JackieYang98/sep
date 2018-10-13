ec2e71250da617bb2ef8fb7475e9ed540ea7f089
Jackie <computer@home>
2018-10-14 01:03:21 +1100

    Final Live Deployment 5.1.1

---
+++ push.sh
@@ -0,0 +1,16 @@
+config() {
+  git config --global user.email "jackie.j.yang@student.uts.edu.au"
+  git config --global user.name "Jackie via Travis"
+}
+commit() {
+    git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+pushing() {
+  git remote add origin2 https://${GH_TOKEN}@JackieYang98/sep.git
+  git push --quiet --set-upstream origin2
+}
+config
+commit
+pushing
